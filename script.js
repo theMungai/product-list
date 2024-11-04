8b21ee71821c476b95c629747ef55f5962b29609
theMungai <mungaithesimon@gmail.com>
2024-11-04 05:08:04 +0300

    Product List with Cart

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const addCartButton  = document.querySelectorAll(".add-to-cart-button")
+const addCartButton  = document.querySelectorAll(".add-to-cart-button");
+let count = 0
 
 addCartButton.forEach(button => {
     button.addEventListener('click', () => {
-        button.innerHTML = `<i class="fa-solid fa-circle-minus"></i>
-        <span> 0 </span>
-        <i class="fa-solid fa-circle-plus"></i>`
+        button.innerHTML = `<i class="fa-solid fa-circle-minus"></i> <p>${count}</p> <i class="fa-solid fa-circle-plus"></i>`
         button.style.backgroundColor = "hsl(14, 86%, 42%)";
         button.style.color = "white";
         button.classList.add('add-cart-button-after-click');
@@ -14,13 +13,12 @@ addCartButton.forEach(button => {
         
         const plusIcon = button.querySelector(".fa-circle-plus");
         plusIcon.addEventListener('click', () => {
-            let itemQuantity = button.querySelector("span");
-            let body = document.querySelector("body");
-            let count = 0;
-            itemQuantity.innerHTML =  3;
-            console.log("clicked");
-            body.style.backgroundColor = "black"
-            body.style.color = "white"
+
+            if(plusIcon.classList.contains("fa-circle-plus")){
+                count++
+            }
+            const itemQuantity = button.querySelector("p");
+            itemQuantity.textContent = count
 
         })
 
@@ -28,11 +26,15 @@ addCartButton.forEach(button => {
         // accessing minus icon to reduce products in the cart
 
         const minusIcon = button.querySelector(".fa-circle-minus");
-        let body = document.querySelector("body");
         minusIcon.addEventListener('click', () => {
-            body.style.backgroundColor = "white"
-            body.style.color = "green"
+
+            if(minusIcon.classList.contains("fa-circle-minus") && count > 0){
+                count--
+            }
+            const itemQuantity = button.querySelector("p");
+            itemQuantity.textContent = count
         })
+
 
     });
 
