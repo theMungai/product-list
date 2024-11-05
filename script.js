21843e2b6caeb7dc43e4ddef562c7fb863e80295
theMungai <mungaithesimon@gmail.com>
2024-11-05 05:05:23 +0300

    Product List with Cart

--- script.js
+++ script.js
@@ -10,9 +10,25 @@ const cakeButton = document.querySelector("#cake-button");
 const brownieButton = document.querySelector("#brownie-button");
 const pannaCottaButton = document.querySelector("#panna-cotta-button");
 
+
 // Cart
 const emptyCart = document.querySelector(".empty-cart");
-let cartQuantity = document.querySelector("#cart-quantity")
+const cartWithItems = document.querySelector(".cart-with-items")
+let cartQuantity = document.querySelector("#cart-quantity");
+const unorderList = document.querySelector("#unordered-list");
+let listItems = document.createElement("li");
+const removeItemButton = document.querySelector("#remove-item");
+const removeNotification = document.querySelector(".remove-section");
+const body = document.querySelector("body")
+
+removeItemButton.addEventListener('click', () => {
+   unorderList.removeChild(listItems);
+   removeItemButton.style.display = "none"
+   removeNotification.style.display = "block"
+})
+
+
+
 
 
 
@@ -30,14 +46,31 @@ waffleButton.addEventListener('click', () => {
 
      // increasing product when plus icon is clicked
      const plusWaffle = waffleButton.querySelector(".fa-circle-plus");
+     const fullProductWaffle = document.querySelector("#full-product");
+     const calculationOrderedList = document.querySelector(".calculation-unordered-list");
+     const waffleItemNumber = document.querySelector(".number-of-items");
+     let waffleTotalPrice = document.querySelector("#unordered-price-total");
+     const wafflePrice = document.querySelector("#unordered-price");
+
+   //   calculating for the cart
+     waffleItemNumber.innerHTML = `${waffleCount}x`     
+     wafflePrice.innerHTML = `$@${(6.50).toFixed(2)}`    
+     waffleTotalPrice.innerHTML = `${(6.50 * waffleCount).toFixed(2)}`
+
+     calculationOrderedList.append(waffleItemNumber, wafflePrice,waffleTotalPrice);
+     listItems.appendChild(calculationOrderedList)
+     fullProductWaffle.innerHTML = `Waffle with Berries`
+     listItems.append(fullProductWaffle)
+     unorderList.appendChild(listItems)
      plusWaffle.addEventListener('click', () => {
         waffleCount = waffleCount + 1;
         waffleQuantity.textContent = waffleCount
 
-        emptyCart.style.display = 'none'
+        
+        emptyCart.style.display = 'none';
+        cartWithItems.style.display = "block"
+        
         cartQuantity.innerHTML = `${waffleCount}`
-        cartQuantity = cartQuantity + 1;
-        cartQuantity.textContent = waffleCount
      })
      
  
@@ -54,38 +87,89 @@ waffleButton.addEventListener('click', () => {
 
 
 // accessing vanilla bean button
-let vanillaCount = 0;
+let vanillaCount = 0
 vanillaBeanButton.addEventListener('click', () => {
     
-    vanillaBeanButton.innerHTML = `<i class="fa-solid fa-circle-minus"></i> <p>${vanillaCount}</p> <i class="fa-solid fa-circle-plus"></i>`
-    vanillaBeanButton.style.backgroundColor = "hsl(14, 86%, 42%)";
-    vanillaBeanButton.style.color = "white";
-    vanillaBeanButton.classList.add('add-cart-button-after-click');
+   vanillaBeanButton.innerHTML = `<i class="fa-solid fa-circle-minus"></i> <p>${vanillaCount}</p> <i class="fa-solid fa-circle-plus"></i>`;
+   vanillaBeanButton.style.backgroundColor = "hsl(14, 86%, 42%)";
+   vanillaBeanButton.style.color = "white";
+   vanillaBeanButton.classList.add('add-cart-button-after-click');
+
+    const vanillaQuantity = vanillaBeanButton.querySelector("p");
     
+
     // increasing product when plus icon is clicked
-    const vanillaQuantity= vanillaBeanButton.querySelector("p");
     const plusVanilla = vanillaBeanButton.querySelector(".fa-circle-plus");
+    const fullProductVanilla = document.querySelector("#full-product");
+    const calculationOrderedList = document.querySelector(".calculation-unordered-list");
+    const vanillaItemNumber = document.querySelector(".number-of-items");
+    let vanillaTotalPrice = document.querySelector("#unordered-price-total");
+    const vanillaPrice = document.querySelector("#unordered-price");
+
+  //   calculating for the cart
+    vanillaItemNumber.innerHTML = `${vanillaCount}x`     
+    vanillaPrice.innerHTML = `$@${(7.00).toFixed(2)}`    
+    vanillaTotalPrice.innerHTML = `${(7.00 * vanillaCount).toFixed(2)}`
+
+    calculationOrderedList.append(vanillaItemNumber, vanillaPrice,vanillaTotalPrice);
+    listItems.appendChild(calculationOrderedList)
+    fullProductVanilla.innerHTML = `Vanilla Bean Crème Brûlée`
+    listItems.append(fullProductVanilla)
+    unorderList.appendChild(listItems)
     plusVanilla.addEventListener('click', () => {
-       vanillaCount = vanillaCount + 1
+       vanillaCount = vanillaCount + 1;
        vanillaQuantity.textContent = vanillaCount
+
+       
+       emptyCart.style.display = 'none';
+       cartWithItems.style.display = "block"
+       
        cartQuantity.innerHTML = `${vanillaCount}`
-        cartQuantity = cartQuantity + 1;
-        cartQuantity.textContent = vanillaCount
     })
     
 
     // decreasing product when minus icon is clicked
     const minusVanilla = vanillaBeanButton.querySelector(".fa-circle-minus");
     minusVanilla.addEventListener('click', () => {
-
+       
        if(vanillaCount > 0){
-           vanillaCount = vanillaCount - 1
+         vanillaCount = vanillaCount - 1;
        }
+       
     })
+})
+// Popup modal 
+const popup = document.querySelector(".pop-up-modal");
+const confirmButton = document.querySelector(".confirm-order");
+const startNewOrderButton = document.querySelector(".start-new-order");
+const cancelOrderButton = document.querySelector("#cancel-order")
 
+// Showing Popup Modal
+confirmButton.addEventListener('click', () => {
+   if (listItems === 0){
+      popup.style.display = "block"
+   }
 
+   else{
+      alert("Kindly Make Your Order")
+   }
+});
+
+// Hiding Popup Modal
+cancelOrderButton.addEventListener('click', () => {
+   popup.style.display = "none"
 })
 
+startNewOrderButton.addEventListener('click', () => {
+   location.reload(true)
+});
+
+
+
+
+
+
+listItems.focus()
 
 
 
